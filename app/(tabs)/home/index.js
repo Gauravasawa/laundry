f21d8d07fa18c9793dda6e215fea946d1b2afe47
Gauravasawa <asawagaurav3@gmail.com>
2024-03-20 22:02:28 +0530

    added tabs based routing

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Redirect } from "expo-router";
 
 const index = () => {
-  return <Redirect href={"/(tabs)/home"} />;
+  return (
+    <View>
+      <Text>home page</Text>
+    </View>
+  );
 };
 
 export default index;
 
 const styles = StyleSheet.create({});
-
-// -> "/"
