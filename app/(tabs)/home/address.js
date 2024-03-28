90ad03a398f849f770d3dc0d246ed94e0025a954
Gauravasawa <asawagaurav3@gmail.com>
2024-03-28 16:56:33 +0530

    completed the address screen and pickup and delivery slot options

--- app/(tabs)/home/address.js
+++ app/(tabs)/home/address.js
@@ -1,11 +1,274 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Ionicons, Entypo } from "@expo/vector-icons";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import {
+  Ionicons,
+  Entypo,
+  AntDesign,
+  FontAwesome,
+  EvilIcons,
+} from "@expo/vector-icons";
+import moment from "moment";
 
 const address = () => {
-  const handleBack = () => {};
+  const [step, setStep] = useState(1);
+  const [currentDate, setCurrentDate] = useState(moment());
+  const [deliveryDate, setDeliveryDate] = useState(moment());
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [selectedDeliveryTime, setSelectedDeliveryTime] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(moment());
 
-  const handleNext = () => {};
+  const handleBack = () => {
+    setStep((prevStep) => (prevStep > 1 ? prevStep - 1 : prevStep));
+  };
+
+  const pickupTimeOptions = [
+    { startTime: "6:30 AM", endTime: "9:00 AM" },
+    { startTime: "9:00 AM", endTime: "11:30 AM" },
+    { startTime: "5:00 PM", endTime: "7:30 PM" },
+    { startTime: "7:30 PM", endTime: "10:00 PM" },
+  ];
+
+  const handleNext = () => {
+    setStep((prevStep) => {
+      const nextStep = prevStep + 1;
+      console.log("nextStep", nextStep);
+
+      //check if next step is equal to 4
+      if (nextStep === 5) {
+        //call the place order function
+      }
+
+      return nextStep;
+    });
+  };
+
+  console.log("step", step);
+
+  const getNext6Days = () => {
+    const nextDays = [];
+    for (let i = 0; i < 5; i++) {
+      const nextDate = moment(currentDate).add(i, "days");
+
+      nextDays.push(nextDate);
+    }
+    return nextDays;
+  };
+  const renderDateButtons = () => {
+    const next6Days = getNext6Days();
+
+    return next6Days?.map((date, index) => (
+      <TouchableOpacity
+        onPress={() => setSelectedDate(date)}
+        style={{
+          padding: 10,
+          margin: 10,
+          borderRadius: 6,
+          width: 50,
+          backgroundColor: date.isSame(selectedDate, "day")
+            ? "#0066b2"
+            : "white",
+          borderColor: date.isSame(selectedDate, "day") ? "#FEBE10" : "#0066b2",
+          borderWidth: date.isSame(selectedDate, "day") ? 2 : 1,
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 13,
+            color: date.isSame(selectedDate, "day") ? "white" : "black",
+          }}
+        >
+          {date?.format("D")}
+        </Text>
+        <Text
+          style={{
+            marginTop: 3,
+            textAlign: "center",
+            fontSize: 13,
+            color: date.isSame(selectedDate, "day") ? "white" : "black",
+          }}
+        >
+          {date?.format("ddd")}
+        </Text>
+      </TouchableOpacity>
+    ));
+  };
+
+  const getNextDays = () => {
+    const nextDays = [];
+
+    let startDate = moment().add(1, "days");
+
+    if (moment(selectedDate).isSameOrBefore(moment().add(2, "days"), "day")) {
+      startDate = moment(selectedDate).add(2, "days");
+    }
+
+    for (let i = 0; i < 5; i++) {
+      const nextDate = moment(startDate).add(i, "days");
+      nextDays.push(nextDate);
+    }
+
+    return nextDays;
+  };
+
+  const renderButtons = () => {
+    const next6Days = getNextDays();
+
+    return next6Days.map((date, index) => (
+      <TouchableOpacity
+        style={{
+          padding: 10,
+          margin: 10,
+          borderRadius: 6,
+          width: 50,
+          backgroundColor: date.isSame(deliveryDate, "day")
+            ? "#0066b2"
+            : "white",
+          borderColor: date.isSame(deliveryDate, "day") ? "#FEBE10" : "#0066b2",
+          borderWidth: date.isSame(deliveryDate, "day") ? 2 : 1,
+        }}
+        onPress={() => setDeliveryDate(date)}
+        key={index}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            marginTop: 3,
+            fontSize: 13,
+            color: date.isSame(deliveryDate, "day") ? "white" : "black",
+          }}
+        >
+          {date?.format("D")}
+        </Text>
+        <Text
+          style={{
+            textAlign: "center",
+            marginTop: 3,
+            fontSize: 13,
+            color: date.isSame(deliveryDate, "day") ? "white" : "black",
+          }}
+        >
+          {date?.format("ddd")}
+        </Text>
+      </TouchableOpacity>
+    ));
+  };
+
+  const renderPickUpTimeOptions = () => {
+    if (selectedDate) {
+      const isCurrentDate = selectedDate.isSame(currentDate, "day");
+      const currentTime = moment();
+
+      return pickupTimeOptions.map((options, index) => {
+        const startTime = moment(
+          selectedDate?.format("YYYY-MM-DD") + " " + options.startTime,
+          "YYYY-MM-DD LT"
+        );
+
+        //check if the time slot is past the current time
+
+        const isTimeSlotPast = isCurrentDate && startTime.isBefore(currentDate);
+
+        return (
+          <TouchableOpacity
+            onPress={() => {
+              if (!isTimeSlotPast) {
+                setSelectedTime(options);
+              }
+            }}
+            style={{
+              textDecorationLine: isTimeSlotPast ? "line-through" : "none",
+              opacity: isTimeSlotPast ? 0.5 : 1,
+              padding: 10,
+              margin: 10,
+              borderRadius: 5,
+              backgroundColor:
+                selectedTime &&
+                selectedTime.startTime === options.startTime &&
+                selectedTime.endTime === options.endTime
+                  ? "#0066b2"
+                  : "white",
+              borderColor:
+                selectedTime &&
+                selectedTime.startTime === options.startTime &&
+                selectedTime.endTime === options.endTime
+                  ? "#FEBE10"
+                  : "#0066b2",
+              borderWidth:
+                selectedTime &&
+                selectedTime.startTime === options.startTime &&
+                selectedTime.endTime === options.endTime
+                  ? 2
+                  : 1,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 13,
+                color:
+                  selectedTime &&
+                  selectedTime.startTime === options.startTime &&
+                  selectedTime.endTime === options.endTime
+                    ? "white"
+                    : "black",
+              }}
+            >{`${options.startTime} - ${options.endTime}`}</Text>
+          </TouchableOpacity>
+        );
+      });
+    }
+  };
+
+  const renderTimeOptions = () => {
+    return pickupTimeOptions.map((option, index) => {
+      console.log(option);
+      const startTime = moment(
+        selectedDate.format("YYYY-MM-DD") + " " + option.startTime,
+        "YYYY-MM-DD LT"
+      );
+
+      // Check if the time slot is past the current time
+      return (
+        <TouchableOpacity
+          key={index}
+          onPress={() => {
+            setSelectedDeliveryTime(option);
+          }}
+          style={{
+            margin: 10,
+            padding: 10,
+            borderRadius: 5,
+            backgroundColor:
+              selectedDeliveryTime &&
+              selectedDeliveryTime.startTime === option.startTime &&
+              selectedDeliveryTime.endTime === option.endTime
+                ? "#0066b2"
+                : "white",
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color:
+                selectedDeliveryTime &&
+                selectedDeliveryTime.startTime === option.startTime &&
+                selectedDeliveryTime.endTime === option.endTime
+                  ? "white"
+                  : "black",
+            }}
+          >{`${option.startTime} - ${option.endTime}`}</Text>
+        </TouchableOpacity>
+      );
+    });
+  };
 
   return (
     <View style={{ flex: 1 }}>
@@ -111,6 +374,238 @@ const address = () => {
         </Pressable>
       </View>
 
+      <View style={{ backgroundColor: "#F0F8FF", flex: 1, padding: 10 }}>
+        <ScrollView>
+          {step == 1 && (
+            <View>
+              {/* Map Over all the address */}
+              <Pressable
+                style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
+              >
+                <AntDesign name="plus" size={24} color="black" />
+                <Pressable>
+                  <Text style={{ fontSize: 16 }}>Add Address</Text>
+                </Pressable>
+              </Pressable>
+              <View>
+                {/* Map over the address */}
+                <Pressable
+                  style={{
+                    backgroundColor: "white",
+                    padding: 10,
+                    marginVertical: 10,
+                    borderRadius: 15,
+                    borderWidth: 1,
+                    borderColor: "#0066b2",
+                  }}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                      }}
+                    >
+                      <Ionicons
+                        name="location-outline"
+                        size={24}
+                        color="#0066b2"
+                      />
+                      <Text style={{ fontSize: 17, fontWeight: "500" }}>
+                        Home
+                      </Text>
+                    </View>
+                    <FontAwesome name="flag" size={22} color="#0066b2" />
+                  </View>
+                  <Text
+                    style={{
+                      marginTop: 10,
+                      fontSize: 15,
+                      fontWeight: "500",
+                      width: "95%",
+                    }}
+                  >
+                    #43/82 RajHans Colony near om gurdev Mangal karayalya,
+                    hasnapur Road
+                  </Text>
+                  <Text
+                    style={{
+                      marginTop: 6,
+                      color: "grey",
+                      fontSize: 15,
+                      fontWeight: "500",
+                    }}
+                  >
+                    Loni BK, 413736
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
+          )}
+
+          {step == 2 && (
+            <View
+              style={{
+                marginTop: 10,
+                backgroundColor: "white",
+                padding: 10,
+                borderRadius: 10,
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <EvilIcons name="location" size={24} color="black" />
+                <View>
+                  <Text style={{ fontSize: 16 }}>Pick up Slot</Text>
+                  <Text
+                    style={{ marginTop: 4, fontWeight: "500", fontSize: 16 }}
+                  >
+                    {currentDate.format("MMMM YYYY")}
+                  </Text>
+                </View>
+              </View>
+
+              <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+                {renderDateButtons()}
+              </View>
+
+              <Text style={{ marginHorizontal: 10 }}>Pickup Time Options</Text>
+              <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+                {renderPickUpTimeOptions()}
+              </View>
+            </View>
+          )}
+
+          {step == 3 && (
+            <>
+              <View
+                style={{
+                  backgroundColor: "white",
+                  marginTop: 10,
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 10,
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      gap: 10,
+                    }}
+                  >
+                    <EvilIcons name="location" size={24} color="black" />
+                    <Text>Pick up slot</Text>
+                  </View>
+                  <AntDesign name="edit" size={24} color="black" />
+                </View>
+
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <View
+                    style={{
+                      padding: 10,
+                      margin: 10,
+                      borderRadius: 6,
+                      width: 50,
+                      backgroundColor: "#0066b2",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        textAlign: "center",
+                        fontSize: 13,
+                        color: "white",
+                      }}
+                    >
+                      {selectedDate.format("D")}
+                    </Text>
+                    <Text
+                      style={{
+                        textAlign: "center",
+                        color: "white",
+                        marginTop: 3,
+                        fontSize: 13,
+                      }}
+                    >
+                      {selectedDate.format("ddd")}
+                    </Text>
+                  </View>
+
+                  <View
+                    style={{
+                      padding: 10,
+                      borderRadius: 5,
+                      backgroundColor: "#0066b2",
+                    }}
+                  >
+                    <Text
+                      style={{ textAlign: "center", color: "white" }}
+                    >{`${selectedTime.startTime} - ${selectedTime.endTime}`}</Text>
+                  </View>
+                </View>
+              </View>
+
+              <View
+                style={{
+                  backgroundColor: "white",
+                  marginTop: 10,
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  {renderButtons()}
+                </View>
+
+                <Text style={{ marginHorizontal: 10 }}>
+                  Pickup Time Options
+                </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  {renderTimeOptions()}
+                </View>
+              </View>
+            </>
+          )}
+        </ScrollView>
+      </View>
+
       <View
         style={{
           backgroundColor: "white",
@@ -122,6 +617,7 @@ const address = () => {
         }}
       >
         <Pressable
+          disabled={step === 1}
           onPress={handleBack}
           style={{
             backgroundColor: "#d0d0d0",
@@ -145,7 +641,7 @@ const address = () => {
           <Text
             style={{ textAlign: "center", color: "white", fontWeight: "500" }}
           >
-            Next
+            {step === 4 ? "Place Order" : "Next"}
           </Text>
         </Pressable>
       </View>
